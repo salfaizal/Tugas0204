68b0f0765971494cef2d7df36b2c93317e8b2af1
faizal ardiansyah <faizalardiansyah3@gmail.com>
2017-10-24 14:51:57 +0800

    perbaikan

--- tugas0204.cpp
+++ tugas0204.cpp
@@ -9,12 +9,12 @@ int main(){
 	cout << " Luas 1 Box Parket		= "<<Luas1Box<<" m2"<<endl;
 	double JumlahBoxP, BiayaP, BiayaPG, TotalB;
 	JumlahBoxP = LuasRumah/Luas1Box;
-	cout <<" Jumlah Box Parket		= "<<JumlahBoxP<<endl
+	cout <<" Jumlah Box Parket		= "<<JumlahBoxP<<endl;
 	BiayaP = JumlahBoxP*500;
 	cout<< " Biaya Parket (Rp) 		= "<<BiayaP<<endl;
-	BiayaPG = LuasRumah*20
+	BiayaPG = LuasRumah*20;
 	cout<<" Biaya Pasang (rp) 		= " <<BiayaPG<<endl;
-	TotalB = BiayaP+BiayaPG
+	TotalB = BiayaP+BiayaPG;
 	cout<< " Total Biaya (rp)		=" <<TotalB<<endl;
 }
 
