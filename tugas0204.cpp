3b2644358f66e26f8e493c437cb2b5f6aca87d31
faizal ardiansyah <faizalardiansyah3@gmail.com>
2017-10-24 14:41:11 +0800

    menjumlahkan luasrumah dan jumlah parket

---
+++ tugas0204.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double LuasRumah = 42;
+	double LantaiPkt = 193*1215;
+	double Luas1Box = LantaiPkt*8/1000000;
+	cout << " Luas Rumah	= "<<LuasRumah<<" m2"<<endl;
+	cout << " Luas 1 Box Parket		= "<<Luas1Box<<" m2"<<endl;
+	double JumlahBoxP, BiayaP, BiayaPG, TotalB;
+	JumlahBoxP = LuasRumah/Luas1Box;
+}
